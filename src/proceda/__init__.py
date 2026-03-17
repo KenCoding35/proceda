c748f559f84b81d231c16d70634281668ad7cb87
Vivek Haldar <vh@vivekhaldar.com>
2026-03-16 20:03:01 -0700

    Bump version to 0.1.1

--- src/proceda/__init__.py
+++ src/proceda/__init__.py
@@ -15,7 +15,7 @@ from proceda.human import (
 from proceda.session import RunResult, RunSession, RunStatus
 from proceda.skill import Skill, SkillStep, StepMarker
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = [
     "Agent",
